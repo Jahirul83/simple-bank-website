c55d51c1a004bcb8b535424dfe92e1e43679e443
Jahirul83 <jahirulislamcse83.com>
2023-08-19 15:14:26 +0600

    design login page

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 // adding event handler with submit btn
-document.getElementById('submit-btn').addEventListener('click',function(){
+document.getElementById('submit-btn').addEventListener('click', function () {
     // getting email  from email field
     const UserEmail = document.getElementById('user-email');
     const email = UserEmail.value;
@@ -7,14 +7,15 @@ document.getElementById('submit-btn').addEventListener('click',function(){
 
     const UserPass = document.getElementById('user-pass');
     const pass = UserPass.value;
-    
+
     // verifying email and password
 
-    if(email === 'admin@gmail.com' && pass === 'admin')
-    {
+    if (email === 'admin@gmail.com' && pass === 'admin') {
         window.location.href = 'bank.html';
     }
-    else{
-        alert('invalid email or password');
+    else {
+        alert(' invalid !!! \n Email is admin@gmail.com \n Password is admin');
+        UserEmail.value = '';
+        UserPass.value = '';
     }
 })
